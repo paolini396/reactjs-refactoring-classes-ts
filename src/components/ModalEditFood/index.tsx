5f3d81bebba5fb4cf827b56f5fd90e707f26e594
Gabriel Paolini <paolini396@gmail.com>
2021-04-30 12:38:30 -0300

    feat: refactor components to typescript and React hooks.

--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -2,42 +2,45 @@ import { useRef } from "react";
 import { FiCheckSquare } from "react-icons/fi";
 
 import { FormHandles } from "@unform/core";
-
 import { IFood } from "../../types";
+
 import { Form } from "./styles";
 import { Modal } from "../Modal";
 import { Input } from "../Input";
 
-interface ModalAddFoodProps {
+interface ModalEditFoodProps {
   isOpen: boolean;
   setIsOpen: () => void;
-  handleAddFood: (data: IFood) => Promise<void>;
+  handleUpdateFood: (data: IFood) => Promise<void>;
+  editingFood: IFood;
 }
 
-export const ModalAddFood = ({
+export const ModalEditFood = ({
   isOpen,
   setIsOpen,
-  handleAddFood,
-}: ModalAddFoodProps): JSX.Element => {
+  handleUpdateFood,
+  editingFood,
+}: ModalEditFoodProps): JSX.Element => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = (data: IFood) => {
-    handleAddFood(data);
+  const handleSubmit = async (data: IFood) => {
+    await handleUpdateFood(data);
     setIsOpen();
   };
 
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-      <Form ref={formRef} onSubmit={handleSubmit}>
-        <h1>Novo Prato</h1>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
         <Input name="image" placeholder="Cole o link aqui" />
 
         <Input name="name" placeholder="Ex: Moda Italiana" />
         <Input name="price" placeholder="Ex: 19.90" />
 
         <Input name="description" placeholder="Descrição" />
-        <button type="submit" data-testid="add-food-button">
-          <p className="text">Adicionar Prato</p>
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
           <div className="icon">
             <FiCheckSquare size={24} />
           </div>
